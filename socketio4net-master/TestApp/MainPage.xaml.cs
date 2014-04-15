c72f5f1f881c4d56e64f873e7477a9e4d03f97f2
AndrewTheGreat07 <andrei.alexandrescu@oriel.ox.ac.uk>
2014-04-15 19:16:31 +0300

    Added tester
    A test project which needs some mastering.

---
+++ socketio4net-master/TestApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TestApp.Resources;
+using SocketIOClient;
+
+namespace TestApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            SimpleExample();
+        }
+
+        Client socket;
+        String url = "http://216.151.208.237:1337/";
+
+        private void SimpleExample()
+        {
+            socket = new Client(url);
+            socket.Connect();
+        }
+
+    }
+}
